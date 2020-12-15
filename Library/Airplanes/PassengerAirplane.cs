d79a486e8dfd8a3289ad15980d07a6f50bfe4a44
Igor Terekhin <63662226+spannenberger@users.noreply.github.com>
2020-12-15 13:04:33 +0500

    Update PassengerAirplane.cs

--- Library/Airplanes/PassengerAirplane.cs
+++ Library/Airplanes/PassengerAirplane.cs
@@ -10,7 +10,8 @@ namespace Library.Airplanes
         [DataMember]
         public int PassengersCount { get; set; }
         public const int PassengerWeight = 64;
-        public PassengerAirplane(int passengersCount,int flightNumber, List<Crewman> crew, float planeWeight = 23000) : base(flightNumber, planeWeight, crew)
+        public PassengerAirplane(int passengersCount,int flightNumber, List<Crewman> crew, float planeWeight = 23000) 
+        : base(flightNumber, planeWeight, crew)
         {
             PassengersCount = passengersCount;
         }
